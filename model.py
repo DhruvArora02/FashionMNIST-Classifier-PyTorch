bebace985f0ca4087a43a8ddbaa421c70597039e
Dhruv Arora <63711315+DhruvArora02@users.noreply.github.com>
2025-01-13 20:26:04 -0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+def build_model():
+    model = nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(28*28, 128),
+        nn.ReLU(),
+        nn.Linear(128, 64),
+        nn.ReLU(),
+        nn.Linear(64, 10)
+    )
+    return model
